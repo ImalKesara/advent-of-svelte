990404b7d9d24919f32275a742dec545ff969c07
LoneWolf <imalkesara2001@gmail.com>
2024-07-23 12:16:57 +0530

    custom properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		fontFamily: {
+			sansSerif: ['Source Sans Pro'],
+			mono: ['Source Code Pro'],
+			handwriting: ['Kalam', 'cursive']
+		},
+		extend: {
+			colors: {
+				blacked: '#0c0a09',
+				semiBlacked: '#1c1917',
+				greenC: '#22c55e',
+				bgrey: '#1a1919'
+			},
+			gridTemplateColumns: {
+				gridTabs: 'repeat(4,1fr)'
+			},
+			maxWidth: {
+				'8xl': '84rem'
+			},
+			borderWidth: {
+				1: '0.5px'
+			}
+		}
 	},
 	plugins: []
 };
