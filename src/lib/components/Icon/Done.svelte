e34293869551f901d2fab546b7b6efde2eb50785
LoneWolf <imalkesara2001@gmail.com>
2024-07-27 15:03:26 +0530

    done icon

---
+++ src/lib/components/Icon/Done.svelte
@@ -0,0 +1,12 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="1.2em"
+	height="1.2em"
+	viewBox="0 0 24 24"
+	{...$$props}
+>
+	<g fill="none">
+		<circle cx="12" cy="12" r="8" fill="#22c55e" fill-opacity="0.25" />
+		<path stroke="#22c55e" stroke-width="1.2" d="m8.5 11l2.894 2.894a.15.15 0 0 0 .212 0L19.5 6" />
+	</g>
+</svg>
