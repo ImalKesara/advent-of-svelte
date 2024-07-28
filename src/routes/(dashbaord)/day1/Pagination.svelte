59877dbeb6d1cc4918329ae950d3f593cf94f5cd
LoneWolf <imalkesara2001@gmail.com>
2024-07-28 14:34:50 +0530

    dispatch

--- src/routes/(dashbaord)/day1/Pagination.svelte
+++ src/routes/(dashbaord)/day1/Pagination.svelte
@@ -5,8 +5,9 @@
 	export let copyPersons;
 	export let currentPage: number;
 	export let totalPages;
+	let value: number = 10;
 	const dispatch = createEventDispatcher();
-
+	let rows: number[] = [10, 20, 30, 40, 50];
 	function increase() {
 		dispatch('increasePage');
 	}
@@ -20,7 +21,14 @@
 		Total Children(s): <span class="font-thin">{copyPersons.length}</span>
 	</div>
 	<div class="grid grid-cols-12 items-center justify-center border-1 text-sm">
-		<div class=" col-span-4">Rows per page</div>
+		<div class=" col-span-4">
+			<p>Rows per page</p>
+			<select bind:value>
+				{#each rows as row}
+					<option value={row}>{row}</option>
+				{/each}
+			</select>
+		</div>
 		<div class=" col-span-4">Page {currentPage} of {totalPages}</div>
 		<div class=" grid grid-cols-2">
 			<button on:click={decrease}><Leftarrow /></button>
