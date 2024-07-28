2e1dc35ce286c60bd8090cbdf44e5e3aa4fb4d08
LoneWolf <imalkesara2001@gmail.com>
2024-07-28 18:16:52 +0530

    Base Structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,256 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import One from '$lib/components/NumberIcons/One.svelte';
+	import Two from '$lib/components/NumberIcons/Two.svelte';
+	import Three from '$lib/components/NumberIcons/Three.svelte';
+	import Four from '$lib/components/NumberIcons/Four.svelte';
+	import Five from '$lib/components/NumberIcons/Five.svelte';
+	import Six from '$lib/components/NumberIcons/Six.svelte';
+	import Seven from '$lib/components/NumberIcons/Seven.svelte';
+	import Eight from '$lib/components/NumberIcons/Eight.svelte';
+	import Nine from '$lib/components/NumberIcons/Nine.svelte';
+	import Zero from '$lib/components/NumberIcons/Zero.svelte';
+</script>
+
+<section class="  m-auto mt-40 max-w-8xl text-center text-white">
+	<div class="grid grid-cols-4">
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class=" text-lg">Day</p>
+				<p>Completed</p>
+			</div>
+			<a href="./day1"> <One width={100} height={100} /></a>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class=" text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<a href="#"> <Two width={100} height={100} /></a>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class=" text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<a href="#"> <Three width={100} height={100} /></a>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class=" text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<a href="#"> <Four width={100} height={100} /></a>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class=" text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<a href="#"> <Five width={100} height={100} /></a>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class=" text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<a href="#"> <Six width={100} height={100} /></a>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class=" text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<a href="#"> <Seven width={100} height={100} /></a>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class=" text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<a href="#"> <Eight width={100} height={100} /></a>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class=" text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<a href="#"> <Nine width={100} height={100} /> </a>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class="text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<div class="grid grid-cols-2 gap-1">
+				<a href="#"> <One width={100} height={100} /></a>
+				<Zero width={100} height={100} />
+			</div>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class="text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<div class="grid grid-cols-2 gap-1">
+				<a href="#"> <One width={100} height={100} /></a>
+				<One width={100} height={100} />
+			</div>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class="text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<div class="grid grid-cols-2 gap-1">
+				<a href="#"> <One width={100} height={100} /></a>
+				<Two width={100} height={100} />
+			</div>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class="text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<div class="grid grid-cols-2 gap-1">
+				<a href="#"> <One width={100} height={100} /></a>
+				<Three width={100} height={100} />
+			</div>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class="text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<div class="grid grid-cols-2 gap-1">
+				<a href="#"> <One width={100} height={100} /></a>
+				<Four width={100} height={100} />
+			</div>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class="text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<div class="grid grid-cols-2 gap-1">
+				<a href="#"> <One width={100} height={100} /></a>
+				<Five width={100} height={100} />
+			</div>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class="text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<div class="grid grid-cols-2 gap-1">
+				<a href="#"> <One width={100} height={100} /></a>
+				<Six width={100} height={100} />
+			</div>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class="text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<div class="grid grid-cols-2 gap-1">
+				<a href="#"> <One width={100} height={100} /></a>
+				<Seven width={100} height={100} />
+			</div>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class="text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<div class="grid grid-cols-2 gap-1">
+				<a href="#"> <One width={100} height={100} /></a>
+				<Eight width={100} height={100} />
+			</div>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class="text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<div class="grid grid-cols-2 gap-1">
+				<a href="#"> <One width={100} height={100} /></a>
+				<Nine width={100} height={100} />
+			</div>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class="text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<div class="grid grid-cols-2 gap-1">
+				<a href="#"> <Two width={100} height={100} /></a>
+				<Zero width={100} height={100} />
+			</div>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class="text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<div class="grid grid-cols-2 gap-1">
+				<a href="#"> <Two width={100} height={100} /></a>
+				<One width={100} height={100} />
+			</div>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class="text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<div class="grid grid-cols-2 gap-1">
+				<a href="#"> <Two width={100} height={100} /></a>
+				<Two width={100} height={100} />
+			</div>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class="text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<div class="grid grid-cols-2 gap-1">
+				<a href="#"> <Two width={100} height={100} /></a>
+				<Three width={100} height={100} />
+			</div>
+		</div>
+
+		<div class="grid grid-cols-2 items-center">
+			<div>
+				<p class="text-lg">Day</p>
+				<p>Pending</p>
+			</div>
+			<div class="grid grid-cols-2 gap-1">
+				<a href="#"> <Two width={100} height={100} /></a>
+				<Four width={100} height={100} />
+			</div>
+		</div>
+	</div>
+</section>
+
+<style>
+</style>
