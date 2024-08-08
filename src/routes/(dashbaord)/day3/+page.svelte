f3b009252be34c8e9b11ab37e6c8c81df154c6bc
LoneWolf <imalkesara2001@gmail.com>
2024-08-09 00:57:11 +0530

    Load data success (day3)

--- src/routes/(dashbaord)/day3/+page.svelte
+++ src/routes/(dashbaord)/day3/+page.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import { children, loadChildren } from '$lib/stores/daythreeStore.ts';
 
+	onMount(() => {
+		loadChildren();
+	});
 </script>
 
-<div class="m-auto mt-24 grid max-w-7xl grid-cols-2 gap-5 p-2 md:grid-cols-4">hello day 3</div>
+<div class="m-auto mt-24 grid max-w-7xl grid-cols-2 gap-5 p-2 md:grid-cols-4">
+	{#each $children as child}
+		<div>{`${child.name} ${child.weight}`}</div>
+	{/each}
+</div>
