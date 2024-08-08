8950c2898df2377f93c1fdb43b8b1d6fb0525e38
LoneWolf <imalkesara2001@gmail.com>
2024-08-09 00:52:10 +0530

    Day 3 Store set-up

--- src/lib/stores/daythreeStore.ts
+++ src/lib/stores/daythreeStore.ts
@@ -1,1 +1,8 @@
 import { writable } from 'svelte/store';
+import data from '../../../src/seedTwo.json';
+
+export const children = writable<Children[]>([]);
+
+export const loadChildren = () => {
+	children.set(data.childrens);
+};
