6a6397219025314bebeeb3995e803f4375457db5
LoneWolf <imalkesara2001@gmail.com>
2024-08-13 22:32:15 +0530

    arrow logic

--- src/lib/components/DayThree/Accodion.svelte
+++ src/lib/components/DayThree/Accodion.svelte
@@ -1,16 +1,27 @@
 <script lang="ts">
 	export let open = false;
 	import { slide } from 'svelte/transition';
-	const handleClick = () => open = !open
+	import UpArrow from '../Icon/day Three icons/UpArrow.svelte';
+	import DownArrow from '../Icon/day Three icons/DownArrow.svelte';
+	const handleClick = () => (open = !open);
 </script>
 
-<div class="accordion">
+<div class="accordion border-1 border-bgrey p-5">
 	<div class="header flex justify-between px-1">
-		<div class="text  hover:underline">
+		<div class="text hover:underline">
 			<slot name="head"></slot>
 		</div>
+		<div class="text-sm">
+			<slot name="total"></slot>
+		</div>
 
-		<button on:click={handleClick}>+</button>
+		<button on:click={handleClick}>
+			{#if open}
+				<UpArrow />
+			{:else}
+				<DownArrow />
+			{/if}
+		</button>
 	</div>
 	{#if open}
 		<div class="details" transition:slide>
